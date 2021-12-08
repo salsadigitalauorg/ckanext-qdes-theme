24359ccd887927bafb26be850a1b54cd5e45312d
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-12-08 18:36:47 +0100

    Refresh page on unfollow

--- ckanext/qdes_theme/helpers.py
+++ ckanext/qdes_theme/helpers.py
@@ -2,20 +2,21 @@ import ckan.model as model
 import os
 import logging
 
-from ckan.plugins.toolkit import get_action
+import ckan.lib.helpers as h
+import ckan.plugins.toolkit as tk
 from pprint import pformat
 
 log = logging.getLogger(__name__)
 
 
 def get_group_admin_emails(id):
-    admins = get_action('member_list')({}, {'id': id, 'object_type': 'user', 'capacity': 'admin'})
+    admins = tk.get_action('member_list')({}, {'id': id, 'object_type': 'user', 'capacity': 'admin'})
     emails = []
 
     if admins:
         for admin in admins:
             admin_list = list(admin)
-            user_dict = get_action('user_show')({}, {'id': admin_list[0]})
+            user_dict = tk.get_action('user_show')({}, {'id': admin_list[0]})
 
             if user_dict.get('email') or None:
                 emails.append(user_dict.get('email'))
@@ -31,5 +32,5 @@ def return_format_label(resource):
     data_dict['vocabulary_service_name'] = 'format'
     data_dict['term_label'] = None
     data_dict['term_uri'] = resource
-    label = get_action('get_vocabulary_service_term')({}, data_dict)
+    label = tk.get_action('get_vocabulary_service_term')({}, data_dict)
     return label['label']
