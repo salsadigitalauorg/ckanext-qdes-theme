3d67a0cb6e83f6e12949a996c67e0fe56e69d509
Mark Calvert <markcalvert84@gmail.com>
2021-12-20 09:49:03 +1300

    Removed unused references Fromatted file

--- ckanext/qdes_theme/helpers.py
+++ ckanext/qdes_theme/helpers.py
@@ -1,22 +1,19 @@
-import ckan.model as model
 import os
 import logging
 
-import ckan.lib.helpers as h
-import ckan.plugins.toolkit as tk
-from pprint import pformat
+from ckan.plugins.toolkit import get_action
 
 log = logging.getLogger(__name__)
 
 
 def get_group_admin_emails(id):
-    admins = tk.get_action('member_list')({}, {'id': id, 'object_type': 'user', 'capacity': 'admin'})
+    admins = get_action('member_list')({}, {'id': id, 'object_type': 'user', 'capacity': 'admin'})
     emails = []
 
     if admins:
         for admin in admins:
             admin_list = list(admin)
-            user_dict = tk.get_action('user_show')({}, {'id': admin_list[0]})
+            user_dict = get_action('user_show')({}, {'id': admin_list[0]})
 
             if user_dict.get('email') or None:
                 emails.append(user_dict.get('email'))
@@ -27,10 +24,11 @@ def get_group_admin_emails(id):
 def get_ga_tracking_id():
     return os.getenv('GA_TRACKING_ID')
 
-def return_format_label(resource):    
+
+def return_format_label(resource):
     data_dict = {}
     data_dict['vocabulary_service_name'] = 'format'
     data_dict['term_label'] = None
     data_dict['term_uri'] = resource
-    label = tk.get_action('get_vocabulary_service_term')({}, data_dict)
+    label = get_action('get_vocabulary_service_term')({}, data_dict)
     return label['label']
